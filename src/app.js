4de176b76e9ee038c90403313cd41d9c2ec59fa8
Justin Cairns <Jcairns@eagles.bridgewater.edu>
2022-03-07 10:28:39 -0500

    allowed connections from anywhere to the API

--- src/app.js
+++ src/app.js
@@ -1,10 +1,18 @@
 const express = require('express')
+const cors = require('cors')
 require('./db/mongoose')
-
 const userRouter = require('./routers/users')
 
 const app = express()
 app.use(express.json())
+
+app.use(cors())
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.use(userRouter)
 
